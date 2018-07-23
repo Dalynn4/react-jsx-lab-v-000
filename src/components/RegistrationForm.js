56675dc963890803dc6b3907766dec79617ea911
Randall Robinson <keith_adams007@yahoo.com>
2018-07-23 19:44:03 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+class RegistrationForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <inpout type="password" />
+        <button type="submit" />
+        </form>
+    )
+  }
+}

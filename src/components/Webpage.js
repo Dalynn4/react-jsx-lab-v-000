89c56efa629634071db500d9e0bc5110eddcb62f
Randall Robinson <keith_adams007@yahoo.com>
2018-07-23 19:47:07 +0000

    automatically backed up by learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,11 @@
+class Webpage extends Component {
+  render() {
+    return (
+      <div>
+      <h1>The World's Coolest Webpage</h1>
+      < FillerText />
+      < FillerText />
+      </div>
+    )
+  }
+}
